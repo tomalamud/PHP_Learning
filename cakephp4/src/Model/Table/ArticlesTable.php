86657277973f6bc35f2ec7490337d6fa8875f45d
Tomás Elizondo <tomaselizondo@piper.fibertel.com.ar>
2022-02-28 13:56:32 -0300

    finally functional CMS

--- cakephp4/src/Model/Table/ArticlesTable.php
+++ cakephp4/src/Model/Table/ArticlesTable.php
@@ -2,6 +2,8 @@
 // src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
+use Cake\Validation\Validator;
+
 use Cake\ORM\Table;
 // the Text class
 use Cake\Utility\Text;
@@ -21,5 +23,17 @@ class ArticlesTable extends Table
       // trim slug to maximum length defined in schema
       $entity->slug = substr($sluggedTitle, 0, 191);
     }
+  }
+  public function validationDefault(Validator $validator): Validator
+  {
+    $validator
+      ->notEmptyString('title')
+      ->minLength('title', 10)
+      ->maxLength('title', 255)
+
+      ->notEmptyString('body')
+      ->minLength('body', 10);
+
+    return $validator;
   }
 }
--- cakephp4/templates/Articles/index.php
+++ cakephp4/templates/Articles/index.php
@@ -1,14 +1,15 @@
 <h1>Articles</h1>
-<?= $this->Html->link('Add Article', ['action' => 'add']) ?>
+<h4><?= $this->Html->link("Add Article", ['action' => 'add']) ?></h4>
 <table>
     <tr>
         <th>Title</th>
         <th>Created</th>
+        <th>Action</th>
     </tr>
 
-    <!-- Here is where we iterate through our $articles query object, printing out article info -->
+<!-- Here's where we iterate through our $articles query object, printing out article info -->
 
-    <?php foreach ($articles as $article): ?>
+<?php foreach ($articles as $article): ?>
     <tr>
         <td>
             <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
@@ -16,6 +17,15 @@
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
         </td>
+        <td>
+            <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+            <?= $this->Form->postLink(
+                'Delete',
+                ['action' => 'delete', $article->slug],
+                ['confirm' => 'Are you sure?'])
+            ?>
+        </td>
     </tr>
-    <?php endforeach; ?>
+<?php endforeach; ?>
+
 </table>
